a1c70fd2f9d7727a72f612cb1a44df72092c241d
lttb <kenzhaev.artur@gmail.com>
2023-05-20 16:25:10 +0100

    feat(quefrency): update keymap

--- keyboards/quefrency_rev5/keymap.c
+++ keyboards/quefrency_rev5/keymap.c
@@ -3,11 +3,11 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_DEF] = LAYOUT_all(
-    KC_F1, KC_F2,  KC_TILD,  KC_1,    KC_2,    KC_3,    KC_4,         KC_5,   KC_6,  KC_7,         KC_8,    KC_9,    KC_0,    KC_MINS,   KC_EQL,  KC_DEL,  KC_BSPC, KC_HOME,
-    KC_F3, KC_F4,  KC_TAB,   KC_Q,    KC_W,    KC_E,    KC_R,         KC_T,   KC_Y,  KC_U,         KC_I,    KC_O,    KC_P,    KC_LBRC,   KC_RBRC, KC_BSLS, KC_END,
-    KC_F5, KC_F6,  BASE_ESC, KC_A,    KC_S,    KC_D,    KC_F,         KC_G,   KC_H,  KC_J,         KC_K,    KC_L,    KC_ENT,  KC_QUOT,   KC_NUHS, KC_ENT,  KC_PGUP,
-    KC_F7, KC_F8,  KC_LSFT,  KC_NUBS, KC_Z,    KC_X,    KC_C,         KC_V,   KC_B,  KC_N,         KC_M,    KC_COMM, KC_DOT,  BASE_SLSH, KC_SCLN, KC_UP,   KC_PGDN,
-    KC_F9, KC_F10, KC_RCTL,  KC_LCTL, KC_LALT, KC_LGUI, BASE_THUMB_L, BASE_THUMB_L,  MO(1),BASE_THUMB_R,    KC_HYPR, KC_RCTL, KC_RGUI,   KC_LEFT, KC_DOWN, KC_RGHT
+    KC_F1, KC_F2,  KC_TILD,  KC_1,    KC_2,    KC_3,    KC_4,         KC_5,   KC_6,  KC_7,         KC_8,    KC_9,    KC_0,    KC_MINS,   KC_EQL,         KC_DEL,  KC_BSPC, KC_HOME,
+    KC_F3, KC_F4,  KC_TAB,   KC_Q,    KC_W,    KC_E,    KC_R,         KC_T,   KC_Y,  KC_U,         KC_I,    KC_O,    KC_P,    KC_LBRC,   KC_RBRC,        KC_BSLS, KC_END,
+    KC_F5, KC_F6,  BASE_ESC, KC_A,    KC_S,    KC_D,    KC_F,         KC_G,   KC_H,  KC_J,         KC_K,    KC_L,    KC_ENT,  KC_QUOT,   KC_NUHS,        KC_ENT,  KC_PGUP,
+    KC_F7, KC_F8,  KC_LSFT,  KC_NUBS, KC_Z,    KC_X,    KC_C,         KC_V,   KC_B,  KC_N,         KC_M,    KC_COMM, KC_DOT,  BASE_SLSH, SFT_T(KC_SCLN), KC_UP,   KC_PGDN,
+    KC_F9, KC_F10, KC_RCTL,  KC_LCTL, KC_LALT, KC_LGUI, BASE_THUMB_L, BASE_THUMB_L,  MO(1),BASE_THUMB_R,    KC_HYPR, KC_RCTL, KC_RGUI,   KC_LEFT,        KC_DOWN, KC_RGHT
   ),
 
   [_SYM] = LAYOUT_all(
